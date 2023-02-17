7dd910a6004ccba0af42464c7d3894e42ed2b1f6
Daniel Lugo <daniellugofreelancer@gmail.com>
2023-02-17 17:15:03 -0300

    update validator

--- models/usersAppModel.js
+++ models/usersAppModel.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String},
     password:[{type: String, required: true}],
     from: [{type: String, required: true}],
     role: {type: String, required: true},
     logged: {type: String, required: true},
     weight: {type: Number, required: true},
     size:{type: Number, required: true},
-    imgUrl: {type: String, required: true}
+    imgUrl: {type: String}
 
 })
 
